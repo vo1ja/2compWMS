12d4414788393e30e2b790e114c68dce13b9021b
vo1ja <asasin0762@yandex.ru>
2022-12-19 03:14:43 +0200

    Add files to repo

---
+++ Enums.h
@@ -0,0 +1,24 @@
+#pragma once
+
+// Список перечисления для трёх видов таблиц,
+//  которые можно отобразить на форме.
+enum class TableTypes {
+  WAREHOUSE,
+  SHIPMENTS,
+  STOCK
+};
+
+// Список перечисления для трёх типов кнопок "Новый"
+//  на форме добавления товара.
+enum class ButtonNewTypes {
+  TYPE,
+  COUNTRY,
+  SUPPLIER
+};
+
+// Список перечисления для двух
+//  типов ролей пользователей.
+enum class UserRoles {
+  GUEST,
+  USER
+};
